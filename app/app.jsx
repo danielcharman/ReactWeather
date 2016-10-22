74f88b96fc35705e244269edd30e8976fe9a5aa7
Daniel Charman <hello@danielcharman.com>
2016-10-22 21:43:26 +1100

    Added foundation and jQuery

--- app/app.jsx
+++ app/app.jsx
@@ -8,6 +8,10 @@ let Weather = require('Weather');
 let About = require('About');
 let Examples = require('Examples');
 
+//load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
